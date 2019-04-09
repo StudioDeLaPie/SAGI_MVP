b898b037ff4619e1f42611f377b585e746ca46bc
Kyohei <kalops22.kikoo@gmail.com>
2019-04-09 14:58:56 +0200

    Ajout de la Plaque A Pulte

---
+++ ProtoCubesCharges/Assets/Scripts/PlaqueAPulte.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaqueAPulte : MonoBehaviour
+{
+    public GameObject simpleAnimate;
+    public float force = 15000;
+    public float durationApparition = 0.2f;
+
+    void Start()
+    {
+        simpleAnimate.SetActive(false);
+    }
+
+    private IEnumerator AnimationActivation()
+    {
+        float time = Time.time;
+        simpleAnimate.SetActive(true);
+        while (Time.time < time + durationApparition)
+        {
+            yield return null;
+        }
+        simpleAnimate.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rg = other.GetComponent<Rigidbody>();
+
+        if(rg == null)//Si on a pas trouvé de rigidbody c'est qu'il s'agit d'un cube ou autre chose
+        {
+            rg = other.gameObject.GetComponentInParent<Rigidbody>();//On cherche dans les parents un rigidbody
+        }
+
+        if (rg != null) 
+        {            
+            rg.velocity = Vector3.zero;
+            rg.AddForce(Vector3.up * force);
+            StartCoroutine(AnimationActivation());
+        }       
+    }
+}
