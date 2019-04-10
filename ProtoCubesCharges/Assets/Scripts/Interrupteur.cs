f9b745245e7902635ce2b2d76b1fa0eff3c30dee
Kyohei <kalops22.kikoo@gmail.com>
2019-04-10 11:15:30 +0200

    Son joueur sur audio mixer + creation porte interrupteur

---
+++ ProtoCubesCharges/Assets/Scripts/Interrupteur.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interrupteur : MonoBehaviour
+{
+    private PorteInterrupteur porteInterrupteur;
+    private ParticleSystem ps;
+    private bool activated = false;
+
+    public AudioClip soundActivation;
+    public AudioClip soundDesactivation;
+
+    private AudioSource audioSource;
+
+    public void SetPorte(PorteInterrupteur porte) { porteInterrupteur = porte; }
+    public bool IsActivated { get { return activated; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponentInChildren<ParticleSystem>();
+        ps.gameObject.SetActive(true);
+        ps.Stop();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponentInParent<Cube>() != null && !activated)
+        {
+            activated = true;
+            ps.Play();
+            porteInterrupteur.InterrupteurActive();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<Cube>() != null && !activated)
+        {
+            audioSource.PlayOneShot(soundActivation);
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponentInParent<Cube>() != null && activated)
+        {
+            activated = false;
+            ps.Stop();
+            ps.Clear();
+            porteInterrupteur.InterrupteurDesactive();
+            audioSource.PlayOneShot(soundDesactivation);
+        }
+    }
+}
+
---
+++ ProtoCubesCharges/Assets/Scripts/PorteInterrupteur.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PorteInterrupteur : MonoBehaviour
+{
+    public List<Interrupteur> interrupteurs;
+
+    public GameObject goPorte;
+    public float tailleMinimum;
+    public float vitesse = 0.01f;
+
+    private Vector3 scaleInitial;
+    private Coroutine coroutineOuverture;
+    private Coroutine coroutineFermeture;
+
+    public AudioClip soundActive;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        foreach (Interrupteur interrupteur in interrupteurs)
+        {
+            interrupteur.SetPorte(this);
+        }
+        scaleInitial = goPorte.transform.localScale;
+        //Do GameObject set active true;             
+    }
+
+    public void InterrupteurActive()
+    {
+        CheckAlInterrupteurs();
+    }
+
+    public void InterrupteurDesactive()
+    {
+        CheckAlInterrupteurs();
+    }
+
+    private void CheckAlInterrupteurs()
+    {
+        bool allInterrupteursActivated = true;
+        foreach (Interrupteur interrupt in interrupteurs)
+        {
+            if (interrupt.IsActivated == false) allInterrupteursActivated = false;
+        }
+
+        if (allInterrupteursActivated)
+        {
+
+            coroutineOuverture = StartCoroutine(OuverturePorte());
+        }
+        else
+        {
+            coroutineFermeture = StartCoroutine(FermeturePorte());
+        }
+    }
+
+    private IEnumerator OuverturePorte()
+    {
+        if (coroutineFermeture != null)
+            StopCoroutine(coroutineFermeture);
+
+        bool isOpen = false;
+
+        while (isOpen == false)
+        {
+            goPorte.transform.localScale -= new Vector3(vitesse, vitesse, vitesse);
+
+            if (goPorte.transform.localScale.x <= tailleMinimum)
+            {
+                isOpen = true;
+            }
+            yield return null;
+        }
+        goPorte.SetActive(false);
+        coroutineOuverture = null;
+    }
+
+    private IEnumerator FermeturePorte()
+    {
+        if (coroutineOuverture != null)
+            StopCoroutine(coroutineOuverture);
+
+        goPorte.SetActive(true);
+
+        bool isClose = false;
+
+        while (isClose == false)
+        {
+            goPorte.transform.localScale += new Vector3(vitesse,vitesse,vitesse);
+
+            if (goPorte.transform.localScale.x >= scaleInitial.x)
+            {
+                isClose = true;
+            }
+            yield return null;
+        }
+        coroutineFermeture = null;
+    }
+
+}
+
