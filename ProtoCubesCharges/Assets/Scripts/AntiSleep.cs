7518b366ceedbe2754f4fdb201d30c00c6a60bd2
Kyohei <kalops22.kikoo@gmail.com>
2019-04-11 16:12:54 +0200

    script anti sleep

---
+++ ProtoCubesCharges/Assets/Scripts/AntiSleep.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiSleep : MonoBehaviour
+{
+    private Rigidbody rg;
+    public bool active = false;
+
+    private void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+       if(active)
+        {
+            rg.AddForce(new Vector3(0,0,0.00001f));
+        }
+    }
+}
