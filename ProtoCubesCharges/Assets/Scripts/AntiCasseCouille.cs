c8798e3c6559dd97f5612b4aeb15f7dbcffd81c9
Kyohei <kalops22.kikoo@gmail.com>
2019-04-11 13:52:59 +0200

    Creation level 6 - 7 - 8

---
+++ ProtoCubesCharges/Assets/Scripts/AntiCasseCouille.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiCasseCouille : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.SetActive(true);
+    }
+
+    void Start()
+    {
+        gameObject.SetActive(true);    
+    }
+}
