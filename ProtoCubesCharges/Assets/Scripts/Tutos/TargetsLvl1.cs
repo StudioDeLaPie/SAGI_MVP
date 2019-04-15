a018709551d153d7b2b7ba8f053000c58934da9b
Kentarosan <jacquin.yannis@gmail.com>
2019-04-15 17:14:34 +0200

    Création préfabs texte, image, billboard + Ajout d'éléments textuels tuto Lvl 1,3,4,5
    Ajout de tous les éléments textuels pour apprendre au joueur à jouer.

---
+++ ProtoCubesCharges/Assets/Scripts/Tutos/TargetsLvl1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetsLvl1 : MonoBehaviour
+{
+    public GameObject activeTxt;
+    public GameObject deactiveTxt;
+    public GameObject cubeTxt; //Le texte au dessus du cube, on le détruit pour plus qu'il gène
+ 
+    private Target target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GetComponent<Target>();
+        activeTxt.SetActive(false);
+        deactiveTxt.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.IsActivated)
+        {
+            activeTxt.SetActive(true);
+            deactiveTxt.SetActive(false);
+        }
+        else
+        {
+            activeTxt.SetActive(false);
+            deactiveTxt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.root.GetComponent<Cube>() != null && other.transform.root.GetComponentInChildren<RectTransform>() != null && cubeTxt != null)
+            Destroy(cubeTxt);
+    }
+}
