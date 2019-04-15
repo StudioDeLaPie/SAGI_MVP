ae459cdf66748f3f90a5d904efc764d330bcf4c9
Kyohei <kalops22.kikoo@gmail.com>
2019-04-15 15:02:23 +0200

    Ajout des plaque de mort et creation du level 13

---
+++ ProtoCubesCharges/Assets/Scripts/PlaqueMort.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaqueMort : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        }
+    }
+}
