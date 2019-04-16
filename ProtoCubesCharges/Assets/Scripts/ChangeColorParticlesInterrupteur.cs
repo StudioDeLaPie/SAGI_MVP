7c1ff082bd5ba3a85f4e19df87a12e6e98bb323a
Kyohei <kalops22.kikoo@gmail.com>
2019-04-16 14:57:35 +0200

    Modifiaction du prefab interrupteur particules de couleur dynamiques

---
+++ ProtoCubesCharges/Assets/Scripts/ChangeColorParticlesInterrupteur.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorParticlesInterrupteur : MonoBehaviour
+{
+    void Start()
+    {
+        ParticleSystem.MainModule main = GetComponent<ParticleSystem>().main;
+        Color couleur = GetComponentInParent<MeshRenderer>().material.color;
+        couleur.a = 1;
+        main.startColor = couleur;
+        Destroy(this);
+    }
+}
