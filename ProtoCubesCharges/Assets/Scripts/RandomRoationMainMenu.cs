399dbdb66b49b70ae97bba4a9095711be25ecfcb
Kyohei <kalops22.kikoo@gmail.com>
2019-04-17 11:58:12 +0200

    Menu Start terminé

---
+++ ProtoCubesCharges/Assets/Scripts/RandomRoationMainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+using Utilities;
+
+public class RandomRoationMainMenu : MonoBehaviour
+{
+    public float minRotation = 1;
+    public float maxRotation = 8;
+
+    void Start()
+    {
+        GetComponent<AutoMoveAndRotate>().rotateDegreesPerSecond.value = 
+        new Vector3(Aleatoire.AleatoireBetweenFloat(minRotation, maxRotation), 
+                    Aleatoire.AleatoireBetweenFloat(minRotation, maxRotation), 
+                    Aleatoire.AleatoireBetweenFloat(minRotation, maxRotation));       
+    }
+}
