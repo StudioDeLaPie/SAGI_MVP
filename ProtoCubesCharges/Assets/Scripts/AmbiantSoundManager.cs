6c854393997cc8c4ba1a35af8b73dd7188f28fcc
Kyohei <kalops22.kikoo@gmail.com>
2019-04-19 12:02:02 +0200

    Modification du system de son
    Ajout des blancs entre chaque son

--- ProtoCubesCharges/Assets/Scripts/AmbiantSoundManager.cs
+++ ProtoCubesCharges/Assets/Scripts/AmbiantSoundManager.cs
@@ -7,11 +7,8 @@ using Utilities;
 public class AmbiantSoundManager : MonoBehaviour
 {
     public AudioSource audioSource;
-    public List<AudioClip> soundsMenu = new List<AudioClip>();
     public List<AudioClip> soundsLevel = new List<AudioClip>();
 
-    public bool makeAleatoireBlank;
-    public float chanceBlankPourcent = 50;
     public int minSecondOfBlank = 30;
     public int maxSecondOfBlank = 60;
 
@@ -20,7 +17,7 @@ public class AmbiantSoundManager : MonoBehaviour
         if (GameObject.FindGameObjectsWithTag("AmbiantSoundManager").Length <= 1)
         {
             DontDestroyOnLoad(gameObject);
-            LaunchSound(false);
+            LaunchSound();
         }
         else
         {
@@ -30,53 +27,39 @@ public class AmbiantSoundManager : MonoBehaviour
 
     IEnumerator WaitEndClip(AudioClip sound)
     {
-        Debug.Log("On attend : " + sound.length + " Secondes avant le lancement de la prochaine");
+        Debug.Log("On attend : " + sound.length + " Secondes avant le lancement de la prochaine musique");
         yield return new WaitForSeconds(sound.length);
 
         //La musique est terminé
-        LaunchSound(makeAleatoireBlank);
+        StartCoroutine(Wait(Aleatoire.AleatoireBetween(minSecondOfBlank, maxSecondOfBlank))); //On attend
     }
 
     IEnumerator Wait(int seconds)
     {
-        Debug.Log("On attend un blanc de : " + seconds + " Secondes avant le lancement de la prochaine");
+        Debug.Log("On attend un blanc de : " + seconds + " Secondes avant le lancement de la prochaine musique");
         yield return new WaitForSeconds(seconds);
 
         //On a finit d'attendre
-        LaunchSound(makeAleatoireBlank);
+        LaunchSound();
     }
 
-    private void LaunchSound(bool aleatoireIsActive)
+    private void LaunchSound()
     {
         //Debug.Log("Lacement du son");
-        AudioClip clip = NextSound(aleatoireIsActive);
-
-        if (clip != null)
-        {
+        AudioClip clip = NextSound();
+        
             audioSource.clip = clip;
             audioSource.Play();
             StartCoroutine(WaitEndClip(clip));
-        }
-        else
-        {
-            StartCoroutine(Wait(Aleatoire.AleatoireBetween(minSecondOfBlank, maxSecondOfBlank)));
-        }
     }
 
-    private AudioClip NextSound(bool activeAleatoire)
+    private AudioClip NextSound()
     {
-        if (activeAleatoire && Aleatoire.AleatoireBetween(1, 100) < chanceBlankPourcent) //Vrai == musique
-        {
-            return null;
-        }
-        else //Faux == Silence
-        {
             int numSound = Aleatoire.AleatoireBetween(1, soundsLevel.Count - 1); //On selectionne un index aleatoire (sauf le premier élèment)
             AudioClip tempSound = soundsLevel[numSound]; //On stock le clip audio
             soundsLevel.RemoveAt(numSound); //On l'efface de la list
             soundsLevel.Insert(0, tempSound); //on l'insert au debut pour ne pas le réutiliser la prochaine fois
 
             return soundsLevel[0];
-        }
     }
 }
