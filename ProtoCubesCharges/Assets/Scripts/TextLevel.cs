43a89c32921535bc9d388b3e4e9c17a12d40de32
Kyohei <kalops22.kikoo@gmail.com>
2019-04-24 11:16:22 +0200

    Ajout du text Level UI ingame

---
+++ ProtoCubesCharges/Assets/Scripts/TextLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextLevel : MonoBehaviour
+{
+    public TextMeshProUGUI textLevel;
+
+    private void Start()
+    {
+        textLevel.text = "Level " + SceneManager.GetActiveScene().buildIndex;
+    }
+
+    void Update()
+    {
+        textLevel.text = "Level " + SceneManager.GetActiveScene().buildIndex;
+    }
+}
