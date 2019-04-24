a01b0f8f1712397ff0d362aab62439b614afe6ab
Kentarosan <jacquin.yannis@gmail.com>
2019-04-24 10:33:08 +0200

    Modif collider Joueur
    Remplacement de la capsule collider par un cylindre à bords inclinés pour pouvoir se mettre sur un rebord sans glisser instantanement.

--- ProtoCubesCharges/Assets/LoadingScreen.cs
+++ ProtoCubesCharges/Assets/LoadingScreen.cs
@@ -41,7 +41,6 @@ public class LoadingScreen : MonoBehaviour
             Color _color = loadingScreenImage.color;
             float speed = transitionSpeed * Time.deltaTime;
             loadingScreenImage.color = new Color(_color.r, _color.g, _color.b, apparition ? _color.a + speed : _color.a - speed);
-            Debug.Log(loadingScreenImage.color.a);
             isOnTransition = apparition ? loadingScreenImage.color.a < 1 : loadingScreenImage.color.a > 0;
             yield return null;
         }
