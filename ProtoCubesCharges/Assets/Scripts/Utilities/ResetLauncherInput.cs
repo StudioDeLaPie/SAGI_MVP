0ad006863a309286999cf017e47ad0c627db3604
Kyohei <kalops22.kikoo@gmail.com>
2019-04-26 15:17:49 +0200

    Ajout du script reset input pour le build
    Ctrl + R dans le menu start pour Reset
    Relancer le jeu

---
+++ ProtoCubesCharges/Assets/Scripts/Utilities/ResetLauncherInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLauncherInput : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.R) && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)))
+            PlayerPrefs.DeleteAll();
+    }
+}
